5ff19680e1081386c68f8f59ac955986862fae59
ThuTrinh83 <truynhzhen@gmail.com>
2026-03-10 00:37:50 +0700

    Time: 23 ms (51.03%), Space: 13.5 MB (38.49%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -3,4 +3,4 @@ class Solution(object):
         if len(s) != len(t):
             return False
 
-        return sorted(s)==sorted(t) #ham dung de sap xep cac phan tu va tra ve mot danh sach (list) moi
+        return sorted(s)==sorted(t) 
